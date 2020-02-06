21aab6f846a50dd3714b7af0824b70b4f0f11eaa
tfpoivic <tfpoivic@gmail.com>
2020-02-06 23:00:39 +0800

    Delete Url.java

--- app/src/main/java/tw/dp103g3/itfood_shop/main/Url.java
+++
@@ -1,6 +0,0 @@
-package tw.dp103g3.itfood_shop.main;
-
-public class Url {
-    public final static String URL = "http://172.20.10.5:8080/Itfood_Web";
-    public final static String SOCKET_URI = "ws://172.20.10.5:8080/Itfood_Web/OrderSocket/shop";
-}
